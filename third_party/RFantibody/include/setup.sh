a6c3baab39e2f8fa7d4f083ec9db3de2fa8ad1b2
abzb1 <cxv0519@gmail.com>
2025-09-09 04:21:17 +0000

    feat: update Dockerfile and setup script for improved environment setup

--- third_party/RFantibody/include/setup.sh
+++ third_party/RFantibody/include/setup.sh
@@ -7,7 +7,6 @@ SENTINEL="${SENTINEL:-$SENTINEL_DEFAULT}"
 
 echo "[setup.sh] using SENTINEL=$SENTINEL"
 
-# Get the absolute path of the directory where this script is located
 currdir=$(cd `dirname $0` && pwd) &&
 
 mkdir -p $currdir/dgl &&
@@ -15,11 +14,9 @@ cd $currdir/dgl &&
 
 wget https://data.dgl.ai/wheels/torch-2.3/cu118/dgl-2.4.0%2Bcu118-cp310-cp310-manylinux1_x86_64.whl &&
 
-# Build the USAlign binaries
 cd $currdir/USalign &&
 make &&
 
-# Build the Python package
 cd /home &&
 poetry install &&
 
